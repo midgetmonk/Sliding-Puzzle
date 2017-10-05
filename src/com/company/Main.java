2e0937ab70fa30f3244479bb88bd1970f4cd721b
Ryan Long <ryan.long.thomas@gmail.com>
2017-10-05 17:26:19 -0500

    Added basic user input and opening message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.HashMap;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
@@ -15,6 +16,9 @@ public class Main {
         Random rn = new Random();
         final int min = 0;
         final int max = 16;
+        final int boardSize = 4;
+        int nextMove;
+        String nextMoveAsString = new String();
         int R = rn.nextInt(max-min)+min;
 
         // For loop that creates a random number until a value that hasn't been entered comes up
@@ -26,14 +30,23 @@ public class Main {
             boardValues.put(x, possibleBoardValues[R]);
         }
 
-        PrintBoard(boardValues,0);
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Welcome to Ryan's Sliding Puzzle! Please enter your move: ");
+        PrintBoard(boardValues,0,boardSize);
+        nextMove = reader.nextInt();
+
+        if(nextMove < 10){
+            nextMoveAsString = " " + nextMoveAsString.valueOf(nextMove);
+        }else{
+            nextMoveAsString.valueOf(nextMove);
+        }
 
     }
 
     // For loop that iterates through the "boardValues" hashmap and prints the value to the screen
-    public static void PrintBoard(HashMap boardValues, int boardLocation) {
-        for(int x=0; x<4; x++){
-            for(int i=0; i<4; i++){
+    public static void PrintBoard(HashMap boardValues, int boardLocation, int boardSize) {
+        for(int x=0; x<boardSize; x++){
+            for(int i=0; i<boardSize; i++){
                 System.out.print(boardValues.get(boardLocation) + " ");
                 boardLocation++;
             }
