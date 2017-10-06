7d495deb7692e74f7a6c54b5a20231e9ce3048c9
Ryan Long <ryan.long.thomas@gmail.com>
2017-10-06 17:36:04 -0500

    Added inline comments and block comments for the methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
             solvedBoard.put(x, possibleBoardValues[x]);
         }
 
-        int boardLocation = 0;
+        int boardLocation = 0; //Current location of " *"
 
         Random rn = new Random();
         final int min = 0;
@@ -37,6 +37,7 @@ public class Main {
             }
             boardValues.put(x, possibleBoardValues[R]);
 
+            //Keeps track of what key " *" is placed in the HashMap boardValues
             if(possibleBoardValues[R] == " *"){
                 boardLocation = x;
             }
@@ -45,39 +46,43 @@ public class Main {
         Scanner reader = new Scanner(System.in);
         System.out.print("Welcome to Ryan's Sliding Puzzle! Please enter your move: \n");
 
+        //While loop that traps the player until game is solved
         while(!boardValues.equals(solvedBoard)) {
             computePossibleMoves(boardValues, boardLocation);
-            PrintBoard(boardValues, 0, boardSize);
+            PrintBoard(boardValues, boardSize);
             nextMove = reader.nextInt();
 
+            //Changes user input to a String
             if (nextMove < 10) {
                 nextMoveAsString = " " + nextMoveAsString.valueOf(nextMove);
             } else {
                 nextMoveAsString = nextMoveAsString.valueOf(nextMove);
             }
 
+            //This block switches the value that the user entered with the adjacent " *"
             tempKey = computeKeyOfUserInput(nextMoveAsString, boardValues, boardLocation);
             tempValue = boardValues.get(tempKey);
             boardValues.replace(tempKey, " *");
             boardValues.replace(boardLocation, tempValue);
             boardLocation = tempKey;
-            System.out.println(boardLocation);
-
-            //PrintBoard(boardValues, 0, boardSize);
         }
 
 
     }
 
-    // For loop that iterates through the "boardValues" hashmap and prints the value to the screen
-    public static void PrintBoard(HashMap boardValues, int boardLocation, int boardSize) {
+    /*
+    * PrintBoard takes the order of values that make up the current board as a HashMap and creates a X by X grid based on the boardSize
+    * @param: Hashmap<Int, String> boardValues, int boardSize
+    * @return: void
+    */
+    public static void PrintBoard(HashMap boardValues, int boardSize) {
+        int startingPoint = 0;
         for(int x=0; x<boardSize; x++){
             for(int i=0; i<boardSize; i++){
-                System.out.print(boardValues.get(boardLocation) + " ");
-                boardLocation++;
+                System.out.print(boardValues.get(startingPoint) + " ");
+                startingPoint++;
             }
-
-            System.out.println();
+            System.out.println(); //new row
         }
     }
 
